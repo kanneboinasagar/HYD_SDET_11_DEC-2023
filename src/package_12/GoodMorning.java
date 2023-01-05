fefb44fced888055aab17a6e74f76307b0e378ff
Kanneboina Sagar Babu <ksbabu96@gmail.com>
2023-01-05 09:48:26 +0530

    Main branch has been created

---
+++ src/package_12/GoodMorning.java
@@ -0,0 +1,11 @@
+package package_12;
+
+public class GoodMorning 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("GoodMorning");
+		
+	}
+
+}
---
+++ src/package_13/GoodAfternoon.java
@@ -0,0 +1,11 @@
+package package_13;
+
+public class GoodAfternoon 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("GoodAfternoon");
+		
+	}
+
+}
